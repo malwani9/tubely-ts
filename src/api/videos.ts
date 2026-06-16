dc77ed124afd4e357688cb36c8ccb47e35a3f591
malwani <98039304+malwani9@users.noreply.github.com>
2026-06-16 15:19:17 +0300

    Add ratio as a folder for dynamic file path key

--- src/api/videos.ts
+++ src/api/videos.ts
@@ -1,7 +1,7 @@
 import { respondWithJSON } from "./json";
 
 import { type ApiConfig } from "../config";
-import { s3, S3Client, type BunRequest } from "bun";
+import { s3, S3Client, stdout, type BunRequest } from "bun";
 import { getBearerToken, validateJWT } from "../auth";
 import { getVideo, updateVideo } from "../db/videos";
 import { BadRequestError, UserForbiddenError } from "./errors";
@@ -36,22 +36,59 @@ export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
   if (videoFile.type !== "video/mp4") {
     throw new BadRequestError("Unsupported file type. Only MP4 allowed");
   }
+  
+  const tempFilePath = "./temp/temp.mp4";
+  await Bun.write(tempFilePath, videoFile);
 
-  await Bun.write("./temp/temp.mp4", videoFile);
+  const ratio = await getVideoAspectRatio(tempFilePath);
 
   const client = cfg.s3Client;
-  const s3File = client.file(`${videoId}.mp4`);
+  const s3File = client.file(`${ratio}/${videoId}.mp4`);
 
-  await s3File.write(Bun.file("./temp/temp.mp4"), {
+  await s3File.write(Bun.file(tempFilePath), {
     type: "video/mp4",
   });
 
   await Bun.file("./temp/temp.mp4").delete();
   
-  let url  = `https://${cfg.s3Bucket}.s3.eu-north-1.amazonaws.com/${videoId}.mp4`;
+  let url  = `https://${cfg.s3Bucket}.s3.eu-north-1.amazonaws.com/${ratio}/${videoId}.mp4`;
 
   videoMeta.videoURL = url;
   updateVideo(cfg.db, videoMeta);
 
   return respondWithJSON(200, null);
+}
+
+
+async function getVideoAspectRatio(filePath: string) {
+    const process = Bun.spawn(["ffprobe", "-v", "error", "-select_streams", "v:0", "-show_entries", 
+      "stream=width,height", "-of", "json", filePath], {
+        stdout: "pipe",
+        stderr: "pipe"
+      });
+
+      const stdoutText = await new Response(process.stdout).text();
+      const stderrText = await new Response(process.stderr).text();
+
+      if (await process.exited !== 0) {
+        throw  new Error(`ffprobe error: ${stderrText}`);
+      }
+
+      const JsonOutput = JSON.parse(stdoutText);
+      if (!JsonOutput.stream || JsonOutput.stream.length === 0) {
+        throw new Error(`No video stream found`);
+      }
+      const { width, height } = JsonOutput.streams[0];
+
+      const ratio = classifyRatio(width, height);
+      return ratio;
+}
+
+function classifyRatio(width: number, height: number) {
+  const value = width / height;
+  const tolerance = 0.1; // how much drift you'll forgive
+
+  if (Math.abs(value - 16 / 9) <= tolerance) return "landscape";
+  if (Math.abs(value - 9 / 16) <= tolerance) return "portrait";
+  return "other";
 }
